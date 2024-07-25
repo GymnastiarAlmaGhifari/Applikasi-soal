ed1324d5e92751ea994865a267e23eac25748a16
GymnastiarAlmaGhifari <e41211478@student.polije.ac.id>
2024-07-25 23:52:36 +0700

    first commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+import {Button} from "./components/ui/button";
+
+function App() {
+
+  return (
+    <div>
+      <Button>Click me a</Button>
+    </div>
+  );
+}
+
+export default App;
